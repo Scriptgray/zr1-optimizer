70194ca7d9bdbc3a2d3a36b46bb8941b027b61a4
‧˚꒰🌷꒱ Arlette Xz  ₊˚⋆ <angelcortes7r@gmail.com>
2025-11-29 19:55:23 -0600

    Actualizar EventOptimizer.js

--- src/optimizer/EventOptimizer.js
+++ src/optimizer/EventOptimizer.js
@@ -1,8 +1,7 @@
 import { EventEmitter } from 'events';
 
 export class EventOptimizer {
-    constructor(cacheManager) {
-        this.cacheManager = cacheManager;
+    constructor() {
         this.originalEmit = EventEmitter.prototype.emit;
     }
 
@@ -10,13 +9,6 @@ export class EventOptimizer {
         const self = this;
         
         EventEmitter.prototype.emit = function(event, ...args) {
-            const eventKey = `event:${this.constructor.name}:${event}:${JSON.stringify(args)}`;
-            
-            if (self.cacheManager.has(eventKey)) {
-                return true;
-            }
-
-            self.cacheManager.set(eventKey, true, { maxAge: 400 });
             return self.originalEmit.apply(this, [event, ...args]);
         };
     }
