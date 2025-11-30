097f1cc280ae6a9957f747ea2056ef4f11d47624
Abrahan987 <168798723+Abrahan987@users.noreply.github.com>
2025-11-29 19:52:53 -0500

    Add cacheManager to EventOptimizer constructor

--- src/optimizer/EventOptimizer.js
+++ src/optimizer/EventOptimizer.js
@@ -1,7 +1,8 @@
 import { EventEmitter } from 'events';
 
 export class EventOptimizer {
-    constructor() {
+    constructor(cacheManager) {
+        this.cacheManager = cacheManager;
         this.originalEmit = EventEmitter.prototype.emit;
     }
 
@@ -9,6 +10,13 @@ export class EventOptimizer {
         const self = this;
         
         EventEmitter.prototype.emit = function(event, ...args) {
+            const eventKey = `event:${this.constructor.name}:${event}:${JSON.stringify(args)}`;
+            
+            if (self.cacheManager.has(eventKey)) {
+                return true;
+            }
+
+            self.cacheManager.set(eventKey, true, { maxAge: 400 });
             return self.originalEmit.apply(this, [event, ...args]);
         };
     }
