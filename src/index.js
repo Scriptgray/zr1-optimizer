370cc074f0afd0c83e1c9ebe03ab4b0e79ecf436
‧˚꒰🌷꒱ Arlette Xz  ₊˚⋆ <angelcortes7r@gmail.com>
2025-11-29 20:14:22 -0600

    Update src/index.js with new optimizations and event handling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import { Engine } from './core/Engine.js';
+import { speedBooster } from './optimizer/SpeedBooster.js';
+import { memoryOptimizer } from './optimizer/MemoryOptimizer.js';
+import { networkOptimizer } from './optimizer/NetworkOptimizer.js';
+import { eventLoopOptimizer } from './optimizer/EventLoopOptimizer.js';
+import { databaseOptimizer } from './optimizer/DatabaseOptimizer.js';
 
 const zr1Engine = new Engine();
 
+memoryOptimizer.enableAutoCleanup();
+eventLoopOptimizer.enableMonitoring();
+
 export default Engine;
-export { zr1Engine };
+export { 
+    zr1Engine, 
+    speedBooster, 
+    memoryOptimizer, 
+    networkOptimizer, 
+    eventLoopOptimizer, 
+    databaseOptimizer
+};
 
 process.on('SIGINT', () => {
     zr1Engine.shutdown();
+    memoryOptimizer.disable();
+    eventLoopOptimizer.disable();
     process.exit(0);
 });
 
 process.on('SIGTERM', () => {
     zr1Engine.shutdown();
+    memoryOptimizer.disable();
+    eventLoopOptimizer.disable();
     process.exit(0);
 });
