21c2da5a1ad6f17df9b0099f10341c5f1dd335e1
Corvette Script 🏌🏻 <zchpt22zyc@privaterelay.appleid.com>
2025-11-30 14:03:36 -0600

    Actualizar index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import { v8Optimizer } from './optimizer/V8Optimizer.js';
 import { advancedCache } from './optimizer/AdvancedCache.js';
-import { networkTurbo } from './optimizer/NetworkTurbo.js';
-import { speedBooster } from './optimizer/SpeedBooster.js';
-import { memoryOptimizer } from './optimizer/MemoryOptimizer.js';
+import { responseTurbo } from './turbo/ResponseTurbo.js';
+import { speedInjector } from './turbo/SpeedInjector.js';
+import { executionTurbo } from './turbo/ExecutionTurbo.js';
 
 v8Optimizer.enableDeepOptimization();
-memoryOptimizer.start();
 
-console.log('\x1b[31m%s\x1b[0m', '🚀 ZR1 OPTIMIZER AVANZADO ACTIVADO');
+console.log('\x1b[31m%s\x1b[0m', '🚀 ZR1 TURBO ACTIVADO - Velocidad extrema');
 
 export { 
     v8Optimizer,
-    advancedCache, 
-    networkTurbo,
-    speedBooster,
-    memoryOptimizer
+    advancedCache,
+    responseTurbo,
+    speedInjector,
+    executionTurbo
 };
