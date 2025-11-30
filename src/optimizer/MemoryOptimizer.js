621bb584815b745a8ce60518c561f13a6096d027
‧˚꒰🌷꒱ Arlette Xz  ₊˚⋆ <angelcortes7r@gmail.com>
2025-11-29 21:32:14 -0600

    Actualizar MemoryOptimizer.js

--- src/optimizer/MemoryOptimizer.js
+++ src/optimizer/MemoryOptimizer.js
@@ -3,27 +3,49 @@ export class MemoryOptimizer {
         this.cleanupInterval = null;
         this.stats = {
             garbageCollections: 0,
+            memoryFreed: 0,
             optimizations: 0
         };
     }
 
     enableAutoCleanup(interval = 30000) {
         this.cleanupInterval = setInterval(() => {
-            this.forceCleanup();
+            const beforeMem = process.memoryUsage().heapUsed;
+            
+            if (global.gc) {
+                global.gc();
+                this.stats.garbageCollections++;
+            }
+            
+            const afterMem = process.memoryUsage().heapUsed;
+            this.stats.memoryFreed += (beforeMem - afterMem);
+            this.stats.optimizations++;
+            
         }, interval);
     }
 
     forceCleanup() {
+        const beforeMem = process.memoryUsage().heapUsed;
+        
         if (global.gc) {
             global.gc();
             this.stats.garbageCollections++;
         }
+        
+        const afterMem = process.memoryUsage().heapUsed;
+        this.stats.memoryFreed += (beforeMem - afterMem);
         this.stats.optimizations++;
+        
         return this.stats;
     }
 
     getStats() {
-        return { ...this.stats };
+        const memory = process.memoryUsage();
+        return {
+            ...this.stats,
+            currentMemory: Math.round(memory.heapUsed / 1024 / 1024) + ' MB',
+            memoryUsage: ((memory.heapUsed / memory.heapTotal) * 100).toFixed(1) + '%'
+        };
     }
 
     disable() {
